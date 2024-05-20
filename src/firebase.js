44e234ff1ce697f8b06ae230c7b7cd764b4e65b3
Helly Shah <hellyshah018@gmail.com>
2024-05-20 19:19:03 +0530

    hi

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDs7xHFGQ1Ty7LrgK4Y6NvBdz1FL09ovjg",
+    authDomain: "chat-box-mern.firebaseapp.com",
+    projectId: "chat-box-mern",
+    storageBucket: "chat-box-mern.appspot.com",
+    messagingSenderId: "422983169956",
+    appId: "1:422983169956:web:26706ff270c1b88b2f7331"
+  };
