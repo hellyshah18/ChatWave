2c8b957c7a6fc63a174c757fdb32ceaba3f58b0f
Helly Shah <hellyshah018@gmail.com>
2024-05-23 13:22:22 +0530

    hi

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Sidebar from './Sidebar';
 function App() {
   return (
     <div className="App">
-     <h1>Welcome to MERN whatsapp</h1>
       <div className='app_body'>
         <Sidebar />
         <Chat/>
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import './Sidebar.css'
+import { Avatar, IconButton } from '@mui/material';
+import TaskIcon from '@mui/icons-material/Task';
+import ChatIcon from '@mui/icons-material/Chat';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
 function Sidebar() {
   return (
     <div className='sidebar'>
       <div className='sidebar_header'>
+        <Avatar />
         <div className='sidebar_headerRight'>
-
+        <IconButton>
+            <TaskIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
         </div>
       </div>
     </div>
